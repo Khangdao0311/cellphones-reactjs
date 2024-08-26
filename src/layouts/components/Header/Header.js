3f7845f3b19898b3202652eb549fccdc5deda653
khangdao0311 <khangdao0311@gmail.com>
2024-08-26 19:20:48 +0700

    Xây dựng phần Header cho dự án

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -4,6 +4,13 @@ import { Autoplay, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import classNames from 'classnames/bind';
+import { CiViewList } from 'react-icons/ci';
+import { IoIosArrowDown } from 'react-icons/io';
+import { RiMapPinLine } from 'react-icons/ri';
+import { FaSearch } from 'react-icons/fa';
+import { LiaPhoneSolid, LiaShippingFastSolid, LiaShoppingBagSolid } from 'react-icons/lia';
+import { HiOutlineUserCircle, HiXMark } from 'react-icons/hi2';
+
 
 import styles from './Header.module.scss';
 import images from '~/assets/images';
@@ -55,8 +62,60 @@ function Header() {
             </div>
             <div className={cx('container')}>
                 <div className={cx('logo-box')}>
-                    <img src={images.logo} alt="logo" className={cx('logo')}/>
+                    <img src={images.logo} alt="logo" className={cx('logo')} />
                 </div>
+                <button className={cx('button')}>
+                    <CiViewList className={cx('icon')} />
+                    <div className={cx('content')}>Danh mục</div>
+                </button>
+                <button className={cx('button')}>
+                    <RiMapPinLine className={cx('icon')} />
+                    <div className={cx('content')}>
+                        <span>
+                            Xem giá tại <IoIosArrowDown  />
+                        </span>
+                        <span>Hồ Chí Minh</span>
+                    </div>
+                </button>
+
+                <div className={cx('search')}>
+                    <button className={cx('button')}>
+                        <FaSearch className={cx('icon')} />
+                    </button>
+                    <input className={cx('input')} type='text' placeholder='Bạn cần tìm gì ?' />
+                    <button className={cx('clear')}>
+                        <HiXMark className={cx('icon')} />
+                    </button>
+                </div>
+
+                <button className={cx('button','bg-none')}>
+                    <LiaPhoneSolid className={cx('icon')} />
+                    <div className={cx('content')}>
+                        Gọi mua hàng <br /> 1800.2097
+                    </div>
+                </button>
+                <button className={cx('button','bg-none')}>
+                    <RiMapPinLine className={cx('icon')} />
+                    <div className={cx('content')}>
+                        Cửa hàng <br /> gần bạn
+                    </div>
+                </button>
+                <button className={cx('button','bg-none')}>
+                    <LiaShippingFastSolid className={cx('icon')} />
+                    <div className={cx('content')}>
+                        Tra cứu <br /> đơn hàng
+                    </div>
+                </button>
+                <button className={cx('button','bg-none')}>
+                    <LiaShoppingBagSolid className={cx('icon')} />
+                    <div className={cx('content')}>
+                        Giỏ <br /> hàng
+                    </div>
+                </button>
+                <button className={cx('button', 'buttonColumn')}>
+                    <HiOutlineUserCircle className={cx('icon')} />
+                    <div className={cx('content')}>Đăng nhập</div>
+                </button>
             </div>
         </header>
     );
